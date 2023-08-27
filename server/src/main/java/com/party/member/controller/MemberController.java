ab75a78c2235eeb645737f5564e9fa47e909f638
yawn <yawning055@gmail.com>
2023-08-28 01:45:31 +0900

    member설정중

--- server/src/main/java/com/party/member/controller/MemberController.java
+++ server/src/main/java/com/party/member/controller/MemberController.java
@@ -1,4 +1,10 @@
 package com.party.member.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
 public class MemberController {
 }
