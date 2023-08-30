1f59e889d5404fb7b4984807baae52206a90d8b1
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-08-30 18:33:09 +0900

    [FE]feat: 헤더, 풋터 컴포넌트 코드작성
    헤더 컴포넌트에는 로그인 상태에 따라 화면에 보여지는 버튼이 달라질 수 있도록 구현하였음
    
    그리고 알람 아이콘의 경우에는 새로운 소식 상태에 따라 아이콘 모양이 변하게 코드를 작성함
    
    단, 상태 변화에 대한 연결은 아직 하지 못함

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import "./App.css";
+import './App.css';
+//COMPONENTS
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <div>
-        <h2>Hello World!</h2>
+      <Header />
+      <div className="content-body">
+        <div>
+          <h2>Hello World!</h2>
+        </div>
+        <div>
+          <a href="/">HOME</a>
+        </div>
+        <div>
+          <a href="/login">LOGIN</a>
+        </div>
+        <div>
+          <a href="/users/new-user">SIGNUP</a>
+        </div>
+        <div>
+          <a href="/posts/1">1st Post</a>
+        </div>
       </div>
-      <div>
-        <a href="/">HOME</a>
-      </div>
-      <div>
-        <a href="/login">LOGIN</a>
-      </div>
-      <div>
-        <a href="/users/new-user">SIGNUP</a>
-      </div>
-      <div>
-        <a href="/posts/1">1st Post</a>
-      </div>
+      <Footer />
     </>
   );
 }
