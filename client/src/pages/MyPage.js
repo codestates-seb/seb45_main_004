83edb43846d1e84c187d83a407034b792fdd99a5
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-08-31 16:29:19 +0900

    [FE]style: Mypage.js 스타일 변경 및 member의 정보를 가지고 오는  api 호출
    - 요소 간격 등의 조정을 위한 스타일링 변경
    - api 호출 : axios 라이브러리 사용하여 member 정보를 가지고 옴
    - 보완점
      : 마이페이지이기 때문에 로그인에 따른 인증 정보가 필요하다고 생각됨
      : 로그인 정보를 이용할 경우, 전역 상태관리를 통해 해당 데이터를 사용할 수 있는지 궁금함

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,20 +1,42 @@
 import { styled } from 'styled-components';
 import Profile from '../components/Profile';
+import Mytab from '../components/Mytab';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const MyInfoSection = styled.section`
+const MyPageSection = styled.section`
   width: 100vw;
-  height: 17em;
   margin-top: 20px;
+  margin-bottom: 20px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 `;
 
 //레이아웃 부터 잡기!
 const MyPage = () => {
+  const [user, setUser] = useState({});
+
+  const fetchMyInfo = async () => {
+    try {
+      const response = await axios.get(`http://3.39.76.109:8080/members/1`);
+      const myInfo = response.data;
+      setUser(myInfo);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMyInfo();
+  }, []);
+
   return (
-    <MyInfoSection>
-      <Profile />
-    </MyInfoSection>
+    <MyPageSection>
+      <Profile user={user} />
+      <Mytab />
+    </MyPageSection>
   );
 };
 
