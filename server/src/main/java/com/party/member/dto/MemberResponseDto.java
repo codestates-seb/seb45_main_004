2260074d497edbbaaa58aba9bbb1c287ee4bee99
yawn <yawning055@gmail.com>
2023-09-01 15:07:24 +0900

    [BE] feat: 모집글 작성하여도 회원조회 되게 수정

--- server/src/main/java/com/party/member/dto/MemberResponseDto.java
+++ server/src/main/java/com/party/member/dto/MemberResponseDto.java
@@ -1,5 +1,6 @@
 package com.party.member.dto;
 
+import com.party.board.dto.ApplicantResponseDto;
 import com.party.board.entity.Applicant;
 import com.party.bookmark.entity.Bookmark;
 import lombok.Getter;
@@ -13,11 +14,12 @@ import java.util.List;
 @Getter
 public class MemberResponseDto {
     // 추가해야 되는거 팔로우 목록
-    private String memberId;
+    private long memberId;
+    private long applicantId;
     private String nickname;
     private String email;
     private String gender;
     private String introduce;
     private String imageUrl;
-    private List<Bookmark> bookmarks = new ArrayList<>();
+    private List<ApplicantResponseDto> applicants = new ArrayList<>();
 }
