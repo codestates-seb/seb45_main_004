59aed1f2ea5b742d65baef9c88ae86bf6adf011e
Donga1125 <ehddk0133@naver.com>
2023-09-06 01:31:57 +0900

    [BE]feat:클라에 board 생성 시 선택할 수 있는 이미지 URL 전달을 위한 목업 설정, AWS 환경변수 수정

--- server/src/main/java/com/party/image/controller/ImageController.java
+++ server/src/main/java/com/party/image/controller/ImageController.java
@@ -4,9 +4,15 @@ import com.party.image.service.AwsService;
 import com.party.member.entity.Member;
 import com.party.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,5 +30,21 @@ public class ImageController {
         member.setImageUrl(imagePath);
         System.out.println(imagePath);
         return imagePath;
+    }
+
+    //board 이미지 전체 전달
+    @GetMapping("/cards")
+    public List<String> getBoardImages(){
+        List<String> boardImageList = new ArrayList<>();
+
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Culture1.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Culture2.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Culture3.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Education1.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Education2.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Education3.png");
+        boardImageList.add( "https://celebeeimage.s3.ap-northeast-2.amazonaws.com/board/Category_Leisure2.png");
+
+       return boardImageList;
     }
 }
