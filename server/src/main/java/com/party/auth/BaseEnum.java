30683a51458e9dae7632ef8a0e4e9750eb6b256b
yawning5 <yawning055@gmail.com>
2023-09-07 10:55:24 +0900

    Revert "[BE]docs: kakao redirect url 변경"

--- server/src/main/java/com/party/auth/BaseEnum.java
+++
@@ -1,7 +0,0 @@
-package com.party.auth;
-
-public interface BaseEnum {
-    String getName();
-
-    String getDescription();
-}
--- server/src/main/java/com/party/auth/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.party.auth.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String username;
-    private String password;
-}
