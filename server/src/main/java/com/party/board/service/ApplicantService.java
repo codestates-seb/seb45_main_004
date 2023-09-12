d3f1934b12b54a0e544e5b0dc21d2393129234b8
Donga1125 <ehddk0133@naver.com>
2023-09-12 22:25:05 +0900

    [BE]feat:총 인원수와 현재 인원수가 같으면 모집 마감처리 추가

--- server/src/main/java/com/party/board/service/ApplicantService.java
+++ server/src/main/java/com/party/board/service/ApplicantService.java
@@ -50,6 +50,9 @@ public class ApplicantService {
         int count = board.getCurrentNum();
         if(count < board.getTotalNum()){
             board.setCurrentNum(count+1);
+            if (count == board.getTotalNum()){
+                board.setStatus(Board.BoardStatus.BOARD_STATUS);
+            }
         }else {//인원수 다 찼으면 추가 안함
             throw new BusinessLogicException(ExceptionCode.NOT_ALLOW_PARTICIPATE);
         }
