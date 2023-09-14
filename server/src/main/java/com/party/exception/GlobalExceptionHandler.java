bf57cf29a546c75aee0f550c5725947ea541dfa3
yawn <yawning055@gmail.com>
2023-09-14 23:34:28 +0900

    [BE]feat: 닉네임 중복발생시 클라이언트에 중복된 닉네임 예외 메시지 발송, 자기소개글 20자 이내로 작성, 마이페이지에서 상태와 작성된 순서에 따라 조회되게 변경

---
+++ server/src/main/java/com/party/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.party.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessLogicException.class)
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
